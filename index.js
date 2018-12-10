2bbe45a26c3fac2a11b320da43d70ed197a2a719
Pradyuman <meetupradyuman@gmail.com>
2018-12-10 18:28:55 +0530

    CSS static adder

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var http = require('http');
 var fs =require('fs');
-
+var path = require('path');
 var server = http.createServer(function(req,res){
     console.log('reuest was made: '* req.url);
     res.writeHead(200,{'Content-Type': 'text/html'});
     var myReadStream = fs.createReadStream(__dirname + '/HTML/index.html','utf8');
     myReadStream.pipe(res);
 });
+index.use(express.static(path.join(__dirname, 'Public')));
 server.listen(8080,'127.0.0.1');
