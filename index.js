70cd50135909aa34a13db0e9e6677321725af6df
Pradyuman <meetupradyuman@gmail.com>
2018-12-10 17:59:30 +0530

    Login page made

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-
 var http = require('http');
-var date =require('./js/date')
+var fs =require('fs');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("Date is "+date.myDateTime());
-    res.end();
-}).listen(8080);
+var server = http.createServer(function(req,res){
+    console.log('reuest was made: '* req.url);
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/HTML/index.html','utf8');
+    myReadStream.pipe(res);
+});
+server.listen(8080,'127.0.0.1');
